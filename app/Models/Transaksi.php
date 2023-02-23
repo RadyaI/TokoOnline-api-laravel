f952d75bb9e803709ba47b773d8f9ce7148a9fed
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-23 09:26:51 +0700

    update syntax

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -10,5 +10,14 @@ class Transaksi extends Model
     use HasFactory;
     protected $table = 'transaksis';
     protected $primarykey = 'id';
-    protected $fillable = ['id_siswa','id_barang','tanggal_beli','status','tanggal_dikirim','tanggal_sampai','created_at','updated_at'];
+    protected $fillable = [
+        'id_siswa',
+        'id_barang',
+        'tanggal_beli',
+        'status',
+        'tanggal_dikirim',
+        'tanggal_sampai',
+        'created_at',
+        'updated_at'
+    ];
 }

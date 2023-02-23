11ec1d0de11ef60aecf2fdbe26d75a140dde4df6
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-23 07:48:07 +0700

    add barang function

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Models\barang;
+
+class BarangController extends Controller
+{
+    public function getbarang(){
+        $barang = barang::get();
+            return response()->json($barang);
+    }
+
+    public function pilihbarang($id){
+        $barang = barang::find($id)->get();
+            return response()->json($barang);
+    }
+
+    public function createbarang(Request $request ){
+        $barang = barang::create([
+            'nama_barang' => $request->input('nama_barang'),
+            'foto' => $request->input('foto'),
+            'deskripsi' => $request->input('deskripsi'),
+            'harga' => $request->input('harga'),
+        ]);
+            return response()->json(['Pesan' => 'Sukses menambah barang']);
+    }
+
+    public function updatebarang(Request $request, $id){
+        $barang = barang::find($id)->update([
+            'nama_barang' => $request->input('nama_barang'),
+            'foto' => $request->input('foto'),
+            'deskripsi' => $request->input('deskripsi'),
+            'harga' => $request->input('harga'),
+        ]);
+            return response()->json(['pesan' => 'Sukses update barang']);
+    }
+
+    public function deletesiswa($id){
+        $barang = barang::find($id)->delete();
+            return response()->json(['pesan' => 'Sukses hapus barang']);
+    }
+}

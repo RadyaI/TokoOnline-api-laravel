f81ba8a9608f043275b6d904008d7a3c23752baf
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-23 08:21:08 +0700

    add transaksi route

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//MEMBER
 Route::get('/getmember',[MemberController::class,'getmember']);
 Route::get('/getmember/{id}',[MemberController::class,'pilihmember']);
 Route::post('/createmember', [MemberController::class,'createmember']);
 Route::put('/updatemember/{id}',[MemberController::class,'updatemember']);
 Route::delete('/deletemember/{id}',[MemberController::class,'deletemember']);
 
+//BARANG
 Route::get('/getbarang',[BarangController::class,'getbarang']);
 Route::get('/getbarang/{id}',[BarangController::class,'pilihbarang']);
 Route::post('/createbarang',[BarangController::class,'createbarang']);
 Route::put('/updatebarang/{id}',[BarangController::class,'updatebarang']);
 Route::delete('/deletebarang/{id}',[BarangController::class,'deletebarang']);
+
+//TRANSAKSI
+Route::get('/gettransaksi',[TransaksiController::class,'gettransaksi']);
+Route::get('/gettransaksi/{id}',[TransaksiController::class,'pilihtransaksi']);
+Route::post('/createtransaksi',[TransaksiController::class,'createtransaksi']);
+Route::put('/updatetransaksi/kirim/{id}',[TransaksiController::class,'updatekirim']);
+Route::put('/updatetransaksi/sampai/{id}',[TransaksiController::class,'updatesampai']);
+Route::delete('/deletetransaksi/{id}',[TransaksiController::class,'deletetransaksi']);
+

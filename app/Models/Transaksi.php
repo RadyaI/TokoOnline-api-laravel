e970e8e0fa8e087fb7cc083d05f2853353fd7a2d
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-23 08:14:50 +0700

    update syntax

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     use HasFactory;
+    protected $table = 'transaksis';
+    protected $primarykey = 'id';
+    protected $fillable = ['id_siswa','id_barang','nama_barang','tanggal_beli','status','tanggal_dikirim','tanggal_sampai','created_at','updated_at'];
 }

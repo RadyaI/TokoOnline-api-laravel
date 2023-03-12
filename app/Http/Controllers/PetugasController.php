664528721cca4a334c8ba2e7a3ff6c396d22354c
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-03-12 20:20:49 +0700

    Update

--- app/Http/Controllers/PetugasController.php
+++ app/Http/Controllers/PetugasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Petugas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class PetugasController extends Controller
 {
